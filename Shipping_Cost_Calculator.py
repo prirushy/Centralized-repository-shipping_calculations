728f27fdb8a8e40079b2f2699c9c1f2658df8e18
prirushy <iheartbigapple@gmail.com>
2025-08-06 21:24:35 -0400

    Revert "adding another comment"
    This reverts commit 20d880a788d70e09be58a1add85c521acee2c5da.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,8 +1,6 @@
 # Shipping Cost Calculator
 # Here is a new update by prirushy
 
-# Here is another update by prirushy
-
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
